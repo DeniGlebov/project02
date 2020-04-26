43422467bade9aaaa2e6d8c5c3d47bb704af7fe8
DeniGlebov <deni.glebov@gmail.com>
2020-04-26 11:28:51 +0300

    add hw

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('1')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return ''
+
+
+@app.route('/requirements/')
+def requirements():
+    return 'requirements'
+
+
+@app.route('/generate-users/')
+def generate_users():
+    return 'generate-users'
+
+
+@app.route('/mean/')
+def mean():
+    return 'mean'
+
+
+@app.route('/space/')
+def space():
+    return 'space'
+
+
+if __name__ == '__main__':
+    app.run()
