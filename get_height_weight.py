6ab32099fb3dbcf460ba3aa39a2aa816c882410d
DeniGlebov <deni.glebov@gmail.com>
2020-04-30 17:39:00 +0300

    cl_n

--- get_height_weight.py
+++
@@ -1,7 +0,0 @@
-def height_weight(data):
-    with open('hw.csv') as file:
-        data = file.read()
-        data = data.split('\n')
-        data = data[1:-2]
-
-    return data
--- get_requirements.py
+++
@@ -1,4 +0,0 @@
-def requirements_state():
-    with open('requirements.txt') as file:
-        req = file.read()
-    return req
